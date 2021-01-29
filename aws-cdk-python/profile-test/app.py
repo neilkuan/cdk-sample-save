24867162b22eb4d146df1da69c2234818760f43d
Neil Guan <guan840912@gmail.com>
2021-01-29 12:02:39 +0800

    add profile account id example

---
+++ aws-cdk-python/profile-test/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#from aws_cdk import core
+#from profile_test.profile_test_stack import ProfileTestStack
+import os 
+from aws_cdk import (
+    core as cdk
+)
+
+class ProfileTestStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        cdk.CfnOutput(self, 'accountId',
+                       value=self.account)
+aws_account = env = {'account': os.environ['CDK_DEFAULT_ACCOUNT'],
+                     'region': os.environ['CDK_DEFAULT_REGION']}
+app = cdk.App()
+ProfileTestStack(app, "profile-test", env=aws_account)
+
+app.synth()
