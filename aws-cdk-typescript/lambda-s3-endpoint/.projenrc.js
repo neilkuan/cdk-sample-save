b9985a8b9a18342c094e0e0029447fd1533c1bd7
Neil Kuan <guan840912@gmail.com>
2021-09-22 16:00:49 +0800

    fix tmpl@1.0.5 deps

--- aws-cdk-typescript/lambda-s3-endpoint/.projenrc.js
+++ aws-cdk-typescript/lambda-s3-endpoint/.projenrc.js
@@ -1,8 +1,8 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: "1.120.0",
-  name: "lambda-s3-endpoint",
+  cdkVersion: '1.120.0',
+  name: 'lambda-s3-endpoint',
   cdkDependencies: [
     '@aws-cdk/aws-ec2',
     '@aws-cdk/aws-s3-deployment',
@@ -10,17 +10,18 @@ const project = new AwsCdkTypeScriptApp({
     '@aws-cdk/aws-iam',
     '@aws-cdk/aws-s3',
   ],
-  defaultReleaseBranch: 'master'
+  defaultReleaseBranch: 'master',
 });
 project.package.addField('resolutions', {
-    ws: '7.4.6',
-    'hosted-git-info': '2.8.9',
-    lodash: '4.17.21',
-    netmask: '2.0.1',
-    'trim-newlines': '3.0.1',
-    y18n: '4.0.1',
-    browserslist: '4.16.5',
-    'pac-resolver': '^5.0.0',
-    tar: '6.1.9',
+  'ws': '7.4.6',
+  'hosted-git-info': '2.8.9',
+  'lodash': '4.17.21',
+  'netmask': '2.0.1',
+  'trim-newlines': '3.0.1',
+  'y18n': '4.0.1',
+  'browserslist': '4.16.5',
+  'pac-resolver': '^5.0.0',
+  'tar': '6.1.9',
+  'tmpl': '^1.0.5',
 });
 project.synth();
