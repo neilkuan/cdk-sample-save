fae979647a885b8d36c62582f2fd300e59698dc7
Neil Kuan <guan840912@gmail.com>
2021-10-06 10:33:47 +0800

    apprunner l2 example

---
+++ aws-cdk-typescript/apprunner-l2/service/go.mod
@@ -0,0 +1,5 @@
+module example.com/m
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.4
---
+++ aws-cdk-typescript/apprunner-l2/ss.sh
@@ -0,0 +1,5 @@
+curl -X POST \
+https://pyxyh4h2x7.ap-northeast-1.awsapprunner.com/admin \
+-H 'authorization: Basic Zm9vOmJhcg==' \
+-H 'content-type: application/json' \
+-d '{"value": "cdk apprunner"}'
